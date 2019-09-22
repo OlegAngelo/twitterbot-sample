45509e175f603320616b7e400121635ed0cd3b5c
OlegAngelo <naubalagtas@gmail.com>
2019-09-23 00:20:59 +0800

    my initial twitter bot startup

---
+++ my_twitter_bot.py
@@ -0,0 +1,46 @@
+import tweepy
+import time
+
+print('Loading, please wait')
+
+CONSUMER_KEY = 'RpMgx8PCAZaSoQCQHxiQgxBHR'
+CONSUMER_SECRET = 'oLocKmVRYRORUDeUtcupW5HMJC5h4v5T1qBYxISWXXNK7HMi52'
+ACCESS_KEY = '802495264208482305-uSmKMuA6jJo55PoOKw0frZSVrYYpodt'
+ACCESS_SECRET = 'aieEHMtec42Rr5iofZcUnzoC8qelEGJvOJNGxkTlnAJbN'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+FILE_NAME = 'last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def reply_to_tweets():
+    print('retrieving and replying to tweets...')
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    mentions = api.mentions_timeline(
+                        last_seen_id,
+                        tweet_mode='extended')
+    for mention in reversed(mentions):
+        print(str(mention.id) + ' - ' + mention.full_text)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        if '#sanaall' in mention.full_text.lower():
+            print('found #sanaall')
+            print('responding back in 3, 2, 1 ...')
+            api.update_status('@ ' + mention.user.screen_name + 'Sana oooolllll mag maoy!', mention.id)
+
+while True:
+    reply_to_tweets()
+    time.sleep(15)
